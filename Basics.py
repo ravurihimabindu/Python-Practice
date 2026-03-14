6577e73f3d2be796080c9b6043d92358b765470d
HimaBindu Ravuri <ravurihimabindu@gmail.com>
2026-03-13 22:49:50 -0400

    Python Practice files

---
+++ Basics.py
@@ -0,0 +1,18 @@
+
+#1 create and display
+city = ["Delhi","Mumbai","Banglore","Hyderabad","Chennai"]
+print(city[0],city[4])
+
+#2 Add/ remove
+city.append("Goa")
+print(city)
+
+city.pop(1)
+print(city)
+
+#3 Sorting
+city.sort()
+print(city)
+
+#4 Tuple unpacking
+info = ("Bindu",36,"Detroit")
